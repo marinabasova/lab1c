1bbff55443dc8d32653b97f8142d6f1bd8d7aa81
marina_basova <79000264+marinabasova@users.noreply.github.com>
2021-05-22 08:13:12 +0300

    Lab1

--- Main.java
+++ Main.java
@@ -1,24 +1,32 @@
 import java.util.Scanner;
 
-import static java.lang.Math.*;
-
 public class Main {
-
     public static void main(String[] args) {
-
-        Scanner str = new Scanner(System.in);
-
         double x;
         double y;
-        System.out.print("Print x:");
-        x = str.nextDouble();
-        if (x < -1)
-            y = pow(x, 6) * log10(abs(pow(6, x) - abs(pow((x - 5), x))));
-        else if (x >= -1 & x < 1)
-            y = sin(x / (1 - 2 * pow(x, 2)));
-        else
-            y = asin(1 / pow(x, 4));
-        System.out.print("y(x)=");
-        System.out.printf("%.2f", y);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число x:");
+        x=scanner.nextInt();
+        double y0 = Math.abs(x);
+        double y1 = Math.sqrt(y0);
+        double y2 = y1 + 1;
+        double y3 = y1 / y2;
+        double y4 = Math.asin(Math.pow(y3,5));
+        double y5 = Math.pow(x,2)+1;
+        double y6 = Math.log(y5)/5;
+        double y7 = Math.exp(y6);
+        double y8 = Math.pow(y0,Math.cos(x));
+        double y9 = Math.pow(2,Math.sin(x));
+        double y10 = (y8+y9)/Math.log(2);
+        double y11= Math.log(y10);
+        //double e =Math.log(Math.pow(2,Math.sin(x)+Math.pow(Math.abs(x),Math.cos(x))))/Math.log(2);
+        y= y4+y7+y11;
+        
+      System.out.print(y);
+        if ((x<=5 && x>=-5) && (y>=5 && y<=10))
+        System.out.println("yes");
+    else System.out.println ("no");
     }
+
+
 }
